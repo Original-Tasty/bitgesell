a77c96bbcc35cf004a1d59bfd70ab770b073c3a6
Original-Tasty <quentin.neveu@hotmail.ca>
2021-03-13 02:28:53 -0500

    Update clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -12,7 +12,7 @@
  * for both BGLd and BGL-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("Bitgesell");
 
 /**
  * Client version number
